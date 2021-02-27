2fb0647f6ccb069f994fdb1c348338642f6b14ef
isaquecosta <isaquecostateotonio@hotmail.com>
2021-02-27 14:24:25 -0300

    adicionando pasta backend

---
+++ spe/src/main/java/br/mv/spe/SpeApplication.java
@@ -0,0 +1,13 @@
+package br.mv.spe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeApplication.class, args);
+	}
+
+}

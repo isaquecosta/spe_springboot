d427954c0ecace4779b3d143a1daca0d151d8330
isaquecosta <isaquecostateotonio@hotmail.com.br>
2021-03-03 09:51:01 -0300

    Correçoes

--- speservice/src/main/java/mv/spe/repository/EstabelecimentoRepository.java
+++ speservice/src/main/java/mv/spe/repository/EstabelecimentoRepository.java
@@ -1,7 +1,7 @@
 package mv.spe.repository;
 
 import mv.spe.domain.Estabelecimento;
-import mv.spe.service.dto.EstabelecimentoDTO;
+import mv.spe.service.dto.EstabelecimentoListDTO;
 import mv.spe.service.filter.EstabelecimentoFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,17 +16,16 @@ public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento
     @Query("SELECT CASE WHEN count(p.id) > 0 THEN true ELSE false END FROM Estabelecimento p where p.nome = :nome AND (:id IS NULL OR :id <> p.id)")
     Boolean existsByNome(@Param("nome") String nome, @Param("id") Long id);
 
-
     @Query(
             value =
-                    "SELECT     new mv.spe.service.dto.EstabelecimentoDTO(e.id, e.nome, e.telefone, e.endereco)"
+                    "SELECT     new mv.spe.service.dto.EstabelecimentoListDTO(e.id, e.nome, e.telefone, e.endereco)"
                             + " FROM        Estabelecimento e"
                             + " WHERE       (:#{#filtro.nome} IS NULL OR LOWER(e.nome) LIKE LOWER(CONCAT('%', :#{#filtro.nome}, '%')))",
             countQuery =
                     "SELECT     COUNT(e.id)"
                             + " FROM    Estabelecimento e"
                             + " WHERE   (:#{#filtro.nome} IS NULL OR LOWER(e.nome) LIKE LOWER(CONCAT('%', :#{#filtro.nome}, '%')))")
-    Page<EstabelecimentoDTO> listar(@Param("filtro") EstabelecimentoFilter filtro, Pageable pageable);
+    Page<EstabelecimentoListDTO> listar(@Param("filtro") EstabelecimentoFilter filtro, Pageable pageable);
 
 
 }
